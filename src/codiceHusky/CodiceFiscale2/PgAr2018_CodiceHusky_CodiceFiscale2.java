477efdf11fcfeafab33baea5f341147ece2dc5c7
Daniele Menotti <d.menotti@studenti.unibs.it>
2019-04-05 15:10:17 +0200

    Creato Main

---
+++ src/codiceHusky/CodiceFiscale2/PgAr2018_CodiceHusky_CodiceFiscale2.java
@@ -0,0 +1,10 @@
+package codiceHusky.CodiceFiscale2;
+
+public class PgAr2018_CodiceHusky_CodiceFiscale2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
