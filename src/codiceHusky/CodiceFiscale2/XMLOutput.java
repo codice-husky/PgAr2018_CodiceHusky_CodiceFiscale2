9b6b44e24615aa50502d9bd9f2cfbaac84951f3a
Daniele Menotti <d.menotti@studenti.unibs.it>
2019-04-16 11:37:38 +0200

    Creato output. da testare

--- src/codiceHusky/CodiceFiscale2/XMLOutput.java
+++ src/codiceHusky/CodiceFiscale2/XMLOutput.java
@@ -38,18 +38,36 @@ public class XMLOutput {
     }
     
     public void writePersona(Persona persona) {
-    	writeElement("nome", persona.getNome());
-    	writeElement("cognome", persona.getCognome());
-    	writeElement("sesso", Character.toString(persona.getSesso()));
-    	writeElement("comune_nascita", persona.getComuneNascita());
-    	writeElement("data_nascita", persona.getDataNascita());
-    	writeElement("codice_fiscale", persona.getCodiceFiscale());
+    	try {
+    		xmlWriter.writeStartElement("persona");
+			xmlWriter.writeAttribute("id", Integer.toString(persona.getId()));
+			writeElement("nome", persona.getNome());
+	    	writeElement("cognome", persona.getCognome());
+	    	writeElement("sesso", Character.toString(persona.getSesso()));
+	    	writeElement("comune_nascita", persona.getComuneNascita());
+	    	writeElement("data_nascita", persona.getDataNascita());
+	    	writeElement("codice_fiscale", persona.getCodiceFiscale());
+	    	xmlWriter.writeEndElement();
+		} catch (XMLStreamException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
     public void writeElement(String name, String value) {
     	try {
-    		xmlWriter.writeStartElement("name");
+    		xmlWriter.writeStartElement(name);
 			xmlWriter.writeCharacters(value);
+			xmlWriter.writeEndElement();
+		} catch (XMLStreamException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    public void close() {
+    	try {
+			xmlWriter.writeEndElement();
 			xmlWriter.writeEndElement();
 		} catch (XMLStreamException e) {
 			// TODO Auto-generated catch block
