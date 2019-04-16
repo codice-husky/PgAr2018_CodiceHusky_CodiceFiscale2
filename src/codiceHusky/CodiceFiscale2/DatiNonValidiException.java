c39c31f486c9bafcf9fc6e30c0a3d024bdfd9a7c
Daniele Menotti <d.menotti@studenti.unibs.it>
2019-04-16 16:29:01 +0200

    Cagava il cazzo

--- src/codiceHusky/CodiceFiscale2/DatiNonValidiException.java
+++ src/codiceHusky/CodiceFiscale2/DatiNonValidiException.java
@@ -1,6 +1,10 @@
 package codiceHusky.CodiceFiscale2;
 
 public class DatiNonValidiException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4938959363357025254L;
 	String stringaDatiNonValidi = "ASSENTE";
 
 	public DatiNonValidiException() {
